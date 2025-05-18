76f3a6eb3221d15c0c4902fb3364ee9a037a9426
shahzainfarooqi <142692117+shahzainfarooqi@users.noreply.github.com>
2025-05-18 10:30:18 +0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,17 @@
+class Countdown:
+    def __init__(self, start):
+        self.current = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < 0:
+            raise StopIteration
+        value = self.current
+        self.current -= 1
+        return value
+
+# Example usage in a for-loop
+for number in Countdown(5):
+    print(number)
